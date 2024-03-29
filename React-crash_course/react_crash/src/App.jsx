c63f18306c739c322d59cb8e27259bb54f46098b
sswayam12 <117537746+sswayam12@users.noreply.github.com>
2024-03-29 19:37:34 +0000

    REACT

---
+++ React-crash_course/react_crash/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import thumb from './components/thumb'
+
+const App = () => {
+  return (
+    <div>
+      <thumb/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-crash_course/react_crash/src/components/thumb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const thumb = () => {
+  return (
+    <div>
+      Thumb video
+    </div>
+  )
+}
+
+export default thumb
